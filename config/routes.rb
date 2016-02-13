e207dbbbc4c4a3d795306d6d50be4af599bae5a2
jmcastel <jmcastellucci@gmail.com>
2016-02-13 10:37:59 +0100

    add redirect for the heroku mighty reef 11

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
 
   #redirect the heroku version
   
+  
+
   constraints(:host => /mighty-reef-1500.herokuapp.com/) do
-    match "/(*path)" => redirect {|params, req| "https://www.watchpurist.com/#{params[:path]}"},  via: [:get, :post]
+    match '/(*path)' => redirect { |params, req|
+      query_params = req.params.except(:path)"http://www.watchpurist.com/#{params[:path]}#{query_params.keys.any? ? "?" + query_params.to_query : ""}"}, via: [:get, :post]
   end
 
 
