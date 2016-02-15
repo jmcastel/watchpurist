1eb75829ff92ca67396ab9187e2066d16bad47f8
jmcastel <jmcastellucci@gmail.com>
2016-02-15 10:24:38 +0100

    perfect the metas

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,6 +5,9 @@ class DashboardController < ApplicationController
 	def index
 		@posts = Post.where(user_id: current_user).order('created_at DESC').paginate(:page => params[:page], :per_page => 4)
 		@messages = PrivateMessage.all.order('created_at DESC').paginate(:page => params[:page], :per_page => 5)
+
+		@meta_title = "Watchpurist dashboard"
+		@meta_description = "Watchpurist dashboard : find all your watches and private messages"
 		 
 	end
 
--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,5 +1,7 @@
 class HelloController < ApplicationController
 
 	def index 
+		@meta_description = "hello, welcome on watchpurist ! The best way to sell or buy luxury watches"
+		@meta_title = "Welcome on watchpurist"
 	end
 end
